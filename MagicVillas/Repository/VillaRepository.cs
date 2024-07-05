355b3d36fd327b896d9a72003625b7fd98714d0f
Your Name <you@example.com>
2024-07-05 14:41:16 +0200

    1 ere partie du section 6 --- implementation de interface repository

---
+++ MagicVillas/Repository/VillaRepository.cs
@@ -0,0 +1,49 @@
+﻿using MagicVilla_VillaAPI.Models;
+using MagicVillas.Data;
+using MagicVillas.Repository.IRepository;
+using System.Linq.Expressions;
+
+namespace MagicVillas.Repository
+{
+    public class VillaRepository : IVillaRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public VillaRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+    
+
+
+
+    public Task Create(Villa entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Villa>> Get(Expression<Func<Villa>> filter = null, bool tracked = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Villa>> GetAll(Expression<Func<Villa>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Remove(Villa entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class ApplcationDbContext
+    {
+    }
+}
--- MagicVillas/obj/Debug/net8.0/MagicVillas.AssemblyInfo.cs
+++ MagicVillas/obj/Debug/net8.0/MagicVillas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MagicVillas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd35ecb84a32026ff2d55f217dedd68cceef5f5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MagicVillas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MagicVillas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
